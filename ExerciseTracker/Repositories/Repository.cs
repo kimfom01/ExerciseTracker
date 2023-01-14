ecdd4af77c18947683fd22c4f44dd1f44fac9814
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-01-14 14:39:39 +0300

    Set base repository class methods to be virtual

---
+++ ExerciseTracker/Repositories/Repository.cs
@@ -0,0 +1,48 @@
+using ExerciseTracker.Data;
+using ExerciseTracker.Models;
+
+namespace ExerciseTracker.Repositories;
+
+public abstract class Repository : IRepository
+{
+    private readonly ExerciseContext _exerciseContext;
+
+    public Repository(ExerciseContext exerciseContext)
+    {
+        _exerciseContext = exerciseContext;
+    }
+
+    public virtual IEnumerable<Exercise> GetExercises()
+    {
+        return _exerciseContext.Exercises;
+    }
+
+    public virtual Exercise GetExerciseById(int id)
+    {
+        return _exerciseContext.Exercises.FirstOrDefault(x => x.Id == id);
+    }
+
+    public virtual void AddExercise(Exercise exercise)
+    {
+        _exerciseContext.Add(exercise);
+    }
+
+    public virtual void UpdateExercise(int id, Exercise exercise)
+    {
+        var exerciseToBeUpdated = GetExerciseById(id);
+        exerciseToBeUpdated.StartDate = exercise.StartDate;
+        exerciseToBeUpdated.EndDate = exercise.EndDate;
+        exerciseToBeUpdated.Comments = exercise.Comments;
+    }
+
+    public virtual void DeleteExercise(int id)
+    {
+        var exerciseToBeDeleted = GetExerciseById(id);
+        _exerciseContext.Remove(exerciseToBeDeleted);
+    }
+
+    public virtual int SaveChanges()
+    {
+        return _exerciseContext.SaveChanges();
+    }
+}
