5daa58a1d5d777989f66a67adda5c605c15781cd
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-01-14 14:35:52 +0300

    update db context parameter with sqlite parameter

--- ExerciseTracker/Program.cs
+++ ExerciseTracker/Program.cs
@@ -1,12 +1,16 @@
 ﻿using ExerciseTracker;
-using ExerciseTracker.Data.DataContext;
-using ExerciseTracker.Data.Repositories;
+using ExerciseTracker.Data;
+using ExerciseTracker.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 var services = new ServiceCollection();
 
 services.AddTransient<Startup>();
-services.AddDbContext<ExerciseContext>();
+services.AddDbContext<ExerciseContext>(options =>
+{
+    options.UseSqlite("Data Source=ExerciseTracker.db");
+});
 services.AddTransient<IExerciseRepository, ExerciseRepository>();
 
 var serviceProvider = services.BuildServiceProvider();
