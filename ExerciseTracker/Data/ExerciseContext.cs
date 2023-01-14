4cd9f668b68fe740e074e7766d7b3785ae495733
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-01-14 14:37:19 +0300

    Change the namespace for db context class

--- ExerciseTracker/Data/ExerciseContext.cs
+++ ExerciseTracker/Data/ExerciseContext.cs
@@ -1,12 +1,12 @@
 using ExerciseTracker.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace ExerciseTracker.Data.DataContext;
+namespace ExerciseTracker.Data;
 
 public class ExerciseContext : DbContext
 {
     public DbSet<Exercise> Exercises { get; set; }
-    
+
     public ExerciseContext(DbContextOptions<ExerciseContext> options) : base(options)
     {
     }
