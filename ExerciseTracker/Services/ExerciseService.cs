2b22dbd5858317fbbbc1b268bebd3a185f95b682
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-01-15 22:11:09 +0300

    Update the service methods with prompts

--- ExerciseTracker/Services/ExerciseService.cs
+++ ExerciseTracker/Services/ExerciseService.cs
@@ -1,15 +1,18 @@
 using ExerciseTracker.Models;
 using ExerciseTracker.Repositories;
+using ExerciseTracker.UserInput;
 
 namespace ExerciseTracker.Services;
 
 public class ExerciseService : IExerciseService
 {
     private readonly IExerciseRepository _exerciseRepository;
+    private readonly Input _input;
 
-    public ExerciseService(IExerciseRepository exerciseRepository)
+    public ExerciseService(IExerciseRepository exerciseRepository, Input input)
     {
         _exerciseRepository = exerciseRepository;
+        _input = input;
     }
 
     public List<Exercise> GetAllExercises()
@@ -17,25 +20,37 @@ public class ExerciseService : IExerciseService
         return _exerciseRepository.GetExercises().ToList();
     }
 
-    public Exercise GetExerciseById(int id)
+    public Exercise GetExerciseById()
     {
+        var id = _input.GetId();
+
         return _exerciseRepository.GetExerciseById(id);
     }
 
-    public void RecordNewExercise(Exercise exercise)
+    public void RecordNewExercise()
     {
+        var startDate = DateTime.Now;
+        var exercise = new Exercise { StartDate = startDate };
+
         _exerciseRepository.AddExercise(exercise);
         _exerciseRepository.SaveChanges();
     }
 
-    public void UpdateExistingExercise(int id, Exercise exercise)
+    public void UpdateExistingExercise()
     {
+        var id = _input.GetId();
+        var endDate = DateTime.Now;
+        var comments = _input.GetComments();
+
+        var exercise = new Exercise { EndDate = endDate, Comments = comments };
+
         _exerciseRepository.UpdateExercise(id, exercise);
         _exerciseRepository.SaveChanges();
     }
 
-    public void DeleteExercise(int id)
+    public void DeleteExercise()
     {
+        var id = _input.GetId();
         _exerciseRepository.DeleteExercise(id);
         _exerciseRepository.SaveChanges();
     }
--- ExerciseTracker/Services/IExerciseService.cs
+++ ExerciseTracker/Services/IExerciseService.cs
@@ -4,9 +4,9 @@ namespace ExerciseTracker.Services;
 
 public interface IExerciseService
 {
-    public void RecordNewExercise(Exercise exercise);
-    public void UpdateExistingExercise(int id, Exercise exercise);
+    public void RecordNewExercise();
+    public void UpdateExistingExercise();
     public List<Exercise> GetAllExercises();
-    public Exercise GetExerciseById(int id);
-    public void DeleteExercise(int id);
+    public Exercise GetExerciseById();
+    public void DeleteExercise();
 }
