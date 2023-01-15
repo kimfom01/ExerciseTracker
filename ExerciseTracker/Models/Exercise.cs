7f6177b72e5ab08254fb6d83461724d548f128e4
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-01-15 21:59:47 +0300

    Modify the model with to have optional properties

--- ExerciseTracker/Models/Exercise.cs
+++ ExerciseTracker/Models/Exercise.cs
@@ -4,7 +4,7 @@ public class Exercise
 {
     public int Id { get; set; }
     public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
-    public TimeSpan Duration { get; set; }
-    public string Comments { get; set; }
+    public DateTime? EndDate { get; set; }
+    public TimeSpan? Duration { get; set; }
+    public string? Comments { get; set; }
 }
